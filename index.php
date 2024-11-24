a47b6c2e722000f118b9bae37dc8b8d6c7558ef5
Bijay Kumar Thakur <bijaythakur330@gmail.com>
2024-11-24 16:27:53 +0545

    default index page added

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <?php get_header(); ?>
 
-
-
 <div id="content" class="site-content">
   <div id="primary" class="content-area">
     <main id="main" class="site-main">
-      <p class="test-font"> Main Content </p>
+      <h1><?php esc_html_e('Blog', 'renegade-insurance') ?></h1>
+      <div class="container">
+        <div class="blog-items">
+          <?php
+          if (have_posts()):
+            while (have_posts()) : the_post();
+              get_template_part('template-parts/content');
+            endwhile;
+          ?>
+            <div class="rgi-pagination">
+              <div class="pages new">
+                <?php previous_posts_link(esc_html__("<< Newer posts", 'renegade-insurance')); ?>
+              </div>
+              <div class="pages old">
+                <?php next_posts_link(esc_html__("Older posts >>", 'renegade-insurance')); ?>
+              </div>
+            </div>
+          <?php
+          else: ?>
+            <p><?php esc_html_e('Nothing yet to be displayed!', 'renegade-insurance') ?></p>
+          <?php endif; ?>
+        </div>
+
+      </div>
     </main>
   </div>
 </div>
