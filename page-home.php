78f9159848b49f0dfc0a31e64a9202c7efa51b54
Bijay Kumar Thakur <bijaythakur330@gmail.com>
2024-11-25 15:31:33 +0545

    the franchise section slicing

--- page-home.php
+++ page-home.php
@@ -144,10 +144,122 @@
           </div>
         </div>
 
-
-
       </section>
 
+      <!-- Section 5 the franchise process -->
+
+      <section class="rgi-home-sec-5">
+        <div class="sec-5-wrap ">
+          <h2 class="rgi-container">The Franchise Process</h2>
+
+          <div class="process-wrap">
+            <div class="process-item">
+              <span class="number">1</span>
+              <h3>Introduction</h3>
+              <p>Complete the form below and we will reach out to provide you with more information.</p>
+
+              <button>Contact Form</button>
+            </div>
+
+            <div class="process-item">
+              <span class="number">2</span>
+              <h3>Kick Off</h3>
+              <p>We will schedule an in-person or riemote meeting to review your application and discuss details as we begin our partnership.</p>
+            </div>
+
+            <div class="process-item">
+              <span class="number">3</span>
+              <h3>Discovery Day</h3>
+              <p>Our franchise development representative will schedule your visit to our corporate location. Here, we will review detailed information on our business model, support, and marketing and Tools Available </p>
+
+
+            </div>
+
+            <div class="process-item">
+              <span class="number">4</span>
+              <h3>Training</h3>
+              <p>Our leadership team will train you and your team to prepare you to establish and operate your own Renegade Insurance franchise. </p>
+            </div>
+          </div>
+          <h2 class="rgi-container">Franchise Facts</h2>
+
+          <div class="franchise-facts-wrap rgi-container">
+            <div class="ff-item ff-item-1">
+              <h3>Forget High Franchise Fees:</h3>
+
+              <ul>
+                <li>
+                  <strong>Initial Franchise Fee:</strong><br>
+                  $20,000
+                </li>
+                <li>
+                  <strong>Minimum Estimated Cost:</strong><br>
+                  $51,900 including franchise fee
+                </li>
+              </ul>
+
+            </div>
+
+            <div class="ff-item ff-item-2">
+              <h3>What We Look for In Our Franchisees: </h3>
+
+              <ul>
+                <li>
+                  Entrepreneurial spirit.
+                </li>
+                <li>
+                  Desire for success.
+                </li>
+                <li>
+                  Love of helping customers.
+                </li>
+                <li>
+                  Success in management, sales, and customer service.
+                </li>
+                <li>
+                  The want to put those skills to use in a business of their own, but not on their own.
+                </li>
+              </ul>
+
+            </div>
+
+            <div class="ff-item ff-item-3">
+              <h3>How We Support Our Franchisees </h3>
+
+              <ul>
+                <li>
+                  <strong>Outstanding Training:</strong> Our 2-Week Training.
+                </li>
+
+                <li>
+                  <strong>Program Dives</strong> Into The Details Of Renegade Insurance That Made Us Successful.
+                </li>
+
+                <li>
+                  <strong>Program Dives</strong> Into The Details Of Renegade Insurance That Made Us Successful.
+                </li>
+                <li>
+                  <strong>Program Dives</strong> Into The Details Of Renegade Insurance That Made Us Successful.
+                </li>
+                <li>
+                  <strong>Program Dives</strong> Into The Details Of Renegade Insurance That Made Us Successful.
+                </li>
+                <li>
+                  <strong>Program Dives</strong> Into The Details Of Renegade Insurance That Made Us Successful.
+                </li>
+
+              </ul>
+
+            </div>
+
+
+
+
+
+          </div>
+
+        </div>
+      </section>
 
 
     </main>
